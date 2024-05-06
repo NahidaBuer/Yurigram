9cae9527076cb13933c61e394a789e840b8d6cac
Jiangjin Wang <kaymw@aosc.io>
2024-05-06 00:06:32 -0700

    AOSCOS: fix(settings): use system fonts by default

--- Telegram/SourceFiles/core/core_settings.h
+++ Telegram/SourceFiles/core/core_settings.h
@@ -13,6 +13,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "ui/widgets/chat_filters_tabs_mode.h"
 #include "window/themes/window_themes_embedded.h"
 #include "ui/chat/attach/attach_send_files_way.h"
+#include "ui/style/style_core_font.h"
 #include "base/flags.h"
 #include "emoji.h"
 
@@ -1197,7 +1198,7 @@ private:
 	rpl::variable<bool> _ttlVoiceClickTooltipHidden = false;
 	WindowPosition _ivPosition;
 	WindowPosition _callPanelPosition;
-	QString _customFontFamily;
+	QString _customFontFamily = style::SystemFontTag();
 	bool _systemUnlockEnabled = false;
 	std::optional<bool> _weatherInCelsius;
 	QByteArray _tonsiteStorageToken;
